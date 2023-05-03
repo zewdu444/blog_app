3c8d30209e6140e7962aab8c82900c829775527a
zewdu erkyhun <zewdu444@gmail.com>
2023-05-03 18:03:01 +0300

    Add most_three_recent_posts method

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,4 @@
 class Like < ApplicationRecord
-  belongs_to :user, foreign_key: :author_id, class_name: 'User'
+  belongs_to :author, foreign_key: :author_id, class_name: 'User'
   belongs_to :post, counter_cache: :likes_counter
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,10 @@
 class User < ApplicationRecord
-  has_many :posts ,dependent: :destroy
+  has_many :posts , foreign_key: :author_id, dependent: :destroy
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
 
-  def  most_recent_post(first_name)
-    user =find_by(first_name)
-    user.posts.order(created_at: :desc).limit(3)
+  def most_three_recent_post
+    posts.order(created_at: :desc).limit(3)
   end
 end
 
-first_user = User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.')
