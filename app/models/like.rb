a86c7b5d5158c909f735667b55c13636aeb0db15
zewdu erkyhun <zewdu444@gmail.com>
2023-05-03 18:21:56 +0300

    Add post counter in comment and like class

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,8 @@
 class Like < ApplicationRecord
   belongs_to :author, foreign_key: :author_id, class_name: 'User'
   belongs_to :post, counter_cache: :likes_counter
+
+  def update_post_like_counter
+    post.update(likes_counter: post.likes.count)
+  end
 end
