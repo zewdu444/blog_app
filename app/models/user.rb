d33038fd564f36425914d3c78faa93346dda5dae
zewdu erkyhun <zewdu444@gmail.com>
2023-05-03 18:26:06 +0300

    Modify readme file

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  has_many :posts , foreign_key: :author_id, dependent: :destroy
+  has_many :posts, foreign_key: :author_id, dependent: :destroy
   has_many :comments, foreign_key: :author_id, dependent: :destroy
   has_many :likes, foreign_key: :author_id, dependent: :destroy
 
@@ -7,4 +7,3 @@ class User < ApplicationRecord
     posts.order(created_at: :desc).limit(3)
   end
 end
-
