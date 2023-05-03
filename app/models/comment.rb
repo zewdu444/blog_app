37915bf5d55e26575d9229cb65cd868c50a40c2d
zewdu erkyhun <zewdu444@gmail.com>
2023-05-03 17:13:44 +0300

    Add relationship between tabels

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,4 @@
 class Comment < ApplicationRecord
-  belongs_to :user, foreign_key: :author_id, class_name: 'User'
+  belongs_to :author, foreign_key: :author_id, class_name: 'User'
   belongs_to :post, class_name: 'Post', counter_cache: :comments_counter
 end
