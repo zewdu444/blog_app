f1672a97c936a9660ccc30a58a8c77fc776d9ed4
zewdu erkyhun <zewdu444@gmail.com>
2023-05-08 17:23:10 +0300

    Add post detail index and show

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,11 @@
+class PostsController < ApplicationController
+  def index
+    @users = User.find(params[:id])
+    @posts = @users.posts
+  end
+
+  def show
+    @post=Post.find(params[:id])
+    @comments = @post.comments
+  end
+end
