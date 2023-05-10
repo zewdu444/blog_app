e2a4f361834ca3b5a1f64176aab1152079febe98
zewdu erkyhun <zewdu444@gmail.com>
2023-05-10 13:18:32 +0300

    Move current_user method to application controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,14 @@
 class PostsController < ApplicationController
   def index
-    @user = User.find(params[:id])
+    @user = User.find(params[:user_id])
     @posts = @user.posts
   end
 
   def show
     @post = Post.find(params[:id])
+  end
+
+  def new
+    @post = @current_user.posts.new
   end
 end
