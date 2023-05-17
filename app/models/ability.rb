a17e6b45345e289d68444e08ea9da7804ab64e31
Marko Kermiciev <kermicievmarko@gmail.com>
2023-05-17 20:42:46 +0200

    ability setup

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -6,7 +6,17 @@ class Ability
   def initialize(user)
     # Define abilities for the user here. For example:
     #
-
+    return unless user.present?
+ 
+      can :read, :all
+      can :create, [Comment, Post]
+ 
+      if user.role == "admin"
+       can :manage, :all
+      else
+       can :destroy, Comment, author: user
+       can :destroy, Post, author: user
+     end
 
     # The first argument to `can` is the action you are giving the user
     # permission to do.
