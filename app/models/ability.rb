b01e6fadc58891bc7333f596451d0bc5b76375d1
zewdu erkyhun <zewdu444@gmail.com>
2023-05-17 21:30:01 +0300

    Add show and hide on button

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -6,11 +6,8 @@ class Ability
   def initialize(user)
     # Define abilities for the user here. For example:
     #
-    #   return unless user.present?
-    #   can :read, :all
-    #   return unless user.admin?
-    #   can :manage, :all
-    #
+
+
     # The first argument to `can` is the action you are giving the user
     # permission to do.
     # If you pass :manage it will apply to every action. Other common actions
